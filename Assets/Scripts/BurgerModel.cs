02034a135dd3631eacacb7f9c5ffff023ff12f02
Maxime Nicolas <71704827+Maxime-UwU@users.noreply.github.com>
2023-11-16 11:07:02 +0100

    finish core game + change assets

--- Assets/Scripts/BurgerModel.cs
+++ Assets/Scripts/BurgerModel.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BurgerModel : MonoBehaviour
 {
+    public GameObject breadBPrefab;
+    public GameObject breadPrefab;
+    public GameObject steakPrefab;
+    public GameObject saladePrefab;
+    public GameObject cheesePrefab;
+    private float position = -3f;
+    private float test = 0f;
     public List<int> burger1 = new List<int>();
     public List<int> burger2 = new List<int>();
     public List<int> burger3 = new List<int>();
@@ -12,17 +19,191 @@ public class BurgerModel : MonoBehaviour
     public List<int> burger6 = new List<int>();
     public List<int> burger7 = new List<int>();
     public List<int> burger8 = new List<int>();
+    public List<int> model;
+    public List<GameObject> modelGameObjects = new List<GameObject>();
 
+
+    void RandomNumber()
+    {
+        int randomNumber = Random.Range(1, 9);
+        Debug.Log(randomNumber);
+
+        if(randomNumber == 1)
+        {
+            model = burger1;
+        }
+        else if(randomNumber == 2)
+        {
+            model = burger2;
+        }
+        else if (randomNumber == 3)
+        {
+            model = burger3;
+        }
+        else if (randomNumber == 4)
+        {
+            model = burger4;
+        }
+        else if (randomNumber == 5)
+        {
+            model = burger5;
+        }
+        else if (randomNumber == 6)
+        {
+            model = burger6;
+        }
+        else if (randomNumber == 7)
+        {
+            model = burger7;
+        }
+        else if (randomNumber == 8)
+        {
+            model = burger8;
+        }
+
+
+        foreach (int ingredient in model)
+        {
+            if (ingredient == 1)
+            {
+
+                GameObject bread = Instantiate(breadPrefab, transform.position, transform.rotation);
+                bread.transform.position = new Vector3(-5, position, test);
+                modelGameObjects.Add(bread);
+                position = position+0.5f;
+                test--;
+                Debug.Log("bread");
+
+            }
+            if (ingredient == 5)
+            {
+
+                GameObject breadB = Instantiate(breadBPrefab, transform.position, transform.rotation);
+                breadB.transform.position = new Vector3(-5, position, test);
+                modelGameObjects.Add(breadB);
+                position = position + 0.5f;
+                test--;
+                Debug.Log("breadB");
+
+            }
+            else if(ingredient == 2)
+            {
+                GameObject steak = Instantiate(steakPrefab, transform.position, transform.rotation);
+                steak.transform.position = new Vector3(-5, position, test);
+                modelGameObjects.Add(steak);
+                position = position + 0.5f;
+                test--;
+                Debug.Log("steak");
+
+            }
+            else if (ingredient == 3)
+            {
+                GameObject salade = Instantiate(saladePrefab, transform.position, transform.rotation);
+                salade.transform.position = new Vector3(-5, position, test);
+                modelGameObjects.Add(salade);
+                position = position + 0.5f;
+                test--;
+                Debug.Log("salade");
+
+            }
+            else if (ingredient == 4)
+            {
+                GameObject cheese = Instantiate(cheesePrefab, transform.position, transform.rotation);
+                cheese.transform.position = new Vector3(-5, position, test);
+                modelGameObjects.Add(cheese);
+                position = position + 0.5f;
+                test--;
+                Debug.Log("cheese");
+
+            }
+            
+            Debug.Log("finish create ingredients");
+
+        }
+    }
+
+    public void ResetModel()
+    {
+        foreach (GameObject obj in modelGameObjects)
+        {
+            Destroy(obj);
+            position = -3f;
+            test = 0f;
+        }
+        Debug.Log("object destroyed");
+        Debug.Log("start randomNumber");
+        RandomNumber();
+        Debug.Log("finish randomNumber");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        burger1.Add(5);
+        burger1.Add(2);
+        burger1.Add(3);
+        burger1.Add(4);
+        burger1.Add(1);
+
+        burger2.Add(5);
+        burger2.Add(2);
+        burger2.Add(4);
+        burger2.Add(2);
+        burger2.Add(4);
+        burger2.Add(3);
+        burger2.Add(1);
+
+        burger3.Add(5);
+        burger3.Add(2);
+        burger3.Add(2);
+        burger3.Add(2);
+        burger3.Add(3);
+        burger3.Add(4);
+        burger3.Add(1);
+
+        burger4.Add(5);
+        burger4.Add(3);
+        burger4.Add(3);
+        burger4.Add(2);
+        burger4.Add(3);
+        burger4.Add(2);
+        burger4.Add(4);
+        burger4.Add(1);
+
+        burger5.Add(5);
+        burger5.Add(4);
+        burger5.Add(4);
+        burger5.Add(4);
+        burger5.Add(3);
+        burger5.Add(4);
+        burger5.Add(1);
+
+        burger6.Add(5);
+        burger6.Add(4);
+        burger6.Add(3);
+        burger6.Add(2);
+        burger6.Add(1);
+
+        burger7.Add(5);
+        burger7.Add(2);
+        burger7.Add(4);
+        burger7.Add(3);
+        burger7.Add(1);
+
+        burger8.Add(5);
+        burger8.Add(2);
+        burger8.Add(4);
+        burger8.Add(5);
+        burger8.Add(3);
+        burger8.Add(2);
+        burger8.Add(1);
+
+        RandomNumber();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Ingredients.cs
+++ Assets/Scripts/Ingredients.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class Keybind : MonoBehaviour
 {
     public Transform fallingPoint;
+    public GameObject breadBPrefab;
     public GameObject breadPrefab;
     public GameObject steakPrefab;
     public GameObject saladePrefab;
     public GameObject cheesePrefab;
     public List ingr;
-    private int layer = -3;
+    public BurgerModel burgerModel;
+    private float layer = -3f;
+    private float zIndex = 0f;
+    private bool similarity = true;
     public List<GameObject> allGameObjects = new List<GameObject>();
 
 
+    void ResetIngredients()
+    {
+        Debug.Log("start resetModel");
+        burgerModel.ResetModel();
+        Debug.Log("finish resetModel");
+
+        foreach (GameObject obj in allGameObjects)
+        {
+            Destroy(obj);
+            layer = -3f;
+            zIndex = 0f;
+        }
+        Debug.Log("added ingredients destroyed");
+
+
+        ingr.ingredients.Clear();
+        Debug.Log("list cleared");
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -27,33 +50,46 @@ public class Keybind : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q))
         {
             ingr.ingredients.Add(1);
-            layer++;
+            zIndex--;
+            layer = layer+0.5f;
             GameObject bread = Instantiate(breadPrefab, transform.position, transform.rotation);
-            bread.transform.position = new Vector3(0, layer, 0);
+            bread.transform.position = new Vector3(0, layer, zIndex);
             allGameObjects.Add(bread);
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            ingr.ingredients.Add(5);
+            zIndex--;
+            layer = layer + 0.5f;
+            GameObject breadB = Instantiate(breadBPrefab, transform.position, transform.rotation);
+            breadB.transform.position = new Vector3(0, layer, zIndex);
+            allGameObjects.Add(breadB);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             ingr.ingredients.Add(2);
-            layer++;
+            zIndex--;
+            layer = layer + 0.5f;
             GameObject steak = Instantiate(steakPrefab, transform.position, transform.rotation);
-            steak.transform.position = new Vector3(0, layer, 0);
+            steak.transform.position = new Vector3(0, layer, zIndex);
             allGameObjects.Add(steak);
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
             ingr.ingredients.Add(3);
-            layer++;
+            zIndex--;
+            layer = layer + 0.5f;
             GameObject salade = Instantiate(saladePrefab, transform.position, transform.rotation);
-            salade.transform.position = new Vector3(0, layer, 0);
+            salade.transform.position = new Vector3(0, layer, zIndex);
             allGameObjects.Add(salade);
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
             ingr.ingredients.Add(4);
-            layer++;
+            zIndex--;
+            layer = layer + 0.5f;
             GameObject cheese = Instantiate(cheesePrefab, transform.position, transform.rotation);
-            cheese.transform.position = new Vector3(0, layer, 0);
+            cheese.transform.position = new Vector3(0, layer, zIndex);
             allGameObjects.Add(cheese);
         }
         else if (Input.GetKeyDown(KeyCode.Backspace))
@@ -61,7 +97,8 @@ public class Keybind : MonoBehaviour
             foreach (GameObject obj in allGameObjects)
             {
                 Destroy(obj, 3F);
-                layer = -3;
+                layer = -3f;
+                zIndex = 0f;
             }
 
             ingr.ingredients.Clear();
@@ -69,6 +106,48 @@ public class Keybind : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Return))
         {
 
+
+            if(burgerModel.model.Count != ingr.ingredients.Count)
+            {
+
+                Debug.Log("different");
+                ResetIngredients();
+
+            }
+            else
+            {
+
+
+                for (int i = 0; i < burgerModel.model.Count; i++)
+                {
+
+
+                    if (burgerModel.model[i] != ingr.ingredients[i])
+                    {
+                        similarity = false;
+                        ResetIngredients();
+                        break;
+                    }
+                    else
+                    {
+                        similarity = true;
+
+                    }
+                }
+
+                if(similarity == true)
+                {
+                    Debug.Log("pass");
+                    ResetIngredients();
+
+                }
+                else
+                {
+                    Debug.Log("fuck");
+                    ResetIngredients();
+
+                }
+            }
         }
     }
 }
